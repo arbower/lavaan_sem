4f8c73b03f07aa45f2afb6cfd77daffea3bf5748
Andrew Bower <a.robert.bower@gmail.com>
2024-04-26 16:01:49 -0700

    Initial commit

---
+++ scripts/00_lavaan_vignette.R
@@ -0,0 +1,31 @@
+# Description: Lavaan Vignette and Other SEM Practice
+
+# Author: Andrew Bower
+# Version:26 April, 2024
+
+# Packages
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load("tidyverse", "assertthat", "babynames", "broom", "compare", "DBI",
+"dbplyr", "devtools", "fivethirtyeight", "flexdashboard", "fs", "ggrepel", "googlesheets4",
+"hexbin", "httr", "janitor", "jsonlite", "knitr", "leaflet", "nycflights13", "RPostgreSQL",
+"rvest", "scales", "tidycensus", "usethis", "vroom", "yaml")
+
+# Parameters
+  # Input Data
+
+  # Output Data
+
+  # Load Data
+
+#===============================================================================
+
+# Packages needed for this vignette
+pacman::p_load(lavaan, lavaanPlot)
+
+# Notes: 1) I need to figure out lisrel clone 2) Bayes SEM 3) MLM SEM 4) IRT 5) Other MTMM designs covariance matrices etc.
+
+
+
+
+
+
